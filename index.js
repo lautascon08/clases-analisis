be4259a769a28797550fc96d95191bd44b396858
laura <tlauratascon@gmail.com>
2022-02-28 07:41:20 -0500

    …

---
+++ index.js
@@ -0,0 +1,49 @@
+console.log("hola mundo");
+// comentarios en linea
+/*comentario
+de
+varias
+lineas
+*/
+
+
+var nombreUsuario
+let edadUsuario
+/*Declarar Variables
+*/
+var nombevariable;
+// crear variable sin asignar valor 
+/*
+inicializar variables:
+asignar un valor al momento de crear la variable
+*/
+var gradoEstudiante="10A";
+/*
+asignar valor:
+igualar una variable existente a un nuevo valor
+*/
+edadUsuario=16;
+console.log(edadUsuario);
+
+//Asignar valor desde otra variable 
+var edad=17;
+edadUsuario=edad;
+console.log(edadUsuario);
+
+/*asignar valor desde el teclado:
+existen dos maneras de hacerlo, la primera es por ventana emergente y la segunda es por medio de un formulario en html
+*/
+var valor=prompt("ingrese un valor");
+console.log("la variable vale  " + valor);
+/*
+contantes: son valores que no pueden ser cambiados durante la ejecucion del codigo.las constantes siempre deben ser inicialisadas
+*/
+const PI=3.1416;
+// imprimir por consola
+
+//simple
+console.log("");
+console.log(valor);
+//concatenado
+console.log("" + valor);
+
---
+++ taller 1.js
@@ -0,0 +1,34 @@
+console.log(" Archivo de práctica y pruebas de JavaScript");
+//comentarios en linea
+/*comentario
+de
+varias
+lineas
+*/
+
+var nombreEstudiante;
+var gradoEstudiante;
+var edadEstudiante;
+/*Declarar Variables
+*/
+ nombreEstudiante="maria alejandra delgado"
+// crear variable sin asignar valor 
+/*
+inicializar variables:
+asignar un valor al momento de crear la variable
+*/
+var gradoEstudiante="10A";
+/*
+asignar valor:
+igualar una variable existente a un nuevo valor
+*/
+edadEstudiante=prompt("ingrese un valor");
+console.log(edadUsuario);
+
+console.log("El estudiante nombreEstudiante del grado gradoEstudiante tiene edadEstudiante")
+//comentarios en linea
+/*comentario
+de
+varias
+lineas
+*/
