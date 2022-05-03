6090d280b93bdcddf422d077f885a66f45fa3149
laura <tlauratascon@gmail.com>
2022-05-03 13:42:17 -0500

    evaluacioncondicional

---
+++ evalucioncondicionales.js
@@ -0,0 +1,41 @@
+
+//laura tascon y alejandra delgado//
+
+//primer punto//
+
+var variable = "Mostrar";
+ 
+if (variable > "mostrar"){
+   console.log("La condición es verdadera")
+}
+
+//segundo punto//
+
+var valor=10;
+ 
+if (valor >= 10 && valor <= 15){
+   console.log("El valor es correcto")
+}
+
+//tercer punto//
+
+var numero=12;
+
+if (numero!=7){
+   console.log("El número es diferente que 7");
+}else if(numero===5){
+    console.log('El número es igual a "5" en valor y tipo');
+}else if(numero=5){
+    console.log("El número es igual a 5 en valor");
+}else if(numero>12y<15){
+    console.log("El número es mayor que 12 y menor a 15");
+}
+
+//Respuesta 3.A
+numero=12;
+//Respuesta 3.B
+numero=5;
+//Respuesta 3.C
+numero=5;
+//Respuesta 3.D
+numero=13;
