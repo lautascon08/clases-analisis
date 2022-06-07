6a672ce24b27d481a4ead57ef98b7ec24fb6cea7
laura <tlauratascon@gmail.com>
2022-06-07 13:39:57 -0500

    tallerciclofor.js

---
+++ CicloFor.js
@@ -0,0 +1,30 @@
+CicloFor.js
+for(var i=1;i<=10;i++){
+     console.log(i)
+}
+for(var i=10;i>=1;i--){
+    console.log (i)
+}
+var limite=prompt("ingrse el limite")
+var sumatoria=0
+for(var i=0; i<0limite; i++){
+    sumatoria+0i;
+}
+console.log(sumatoria);
+
+var inicio=prompt("ingrse el inicio");
+var fin=prompt("ingrese el fin")
+
+for(var ¡=0; ¡<arregloNombres.lengt;++){
+    console.log("el nombre "+¡+" es" +arregloNombre[¡]);
+
+var cantNotas = parseInt(prompt("ingrse la cantidad"));
+var arreglaNotas=[];
+for(var i=0; i<cantNotas; i++){
+    var nota=parseFloat(prompt("nota"+(i+1)));
+    arreglaNotas.push(nota);
+}
+
+var promedio=sumatoria/cantNotas;
+console.log("el promedio de notas es" +
+promedio);
