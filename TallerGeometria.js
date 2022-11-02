9b3017183daa953fb146acd83709c4a69e0e806d
laura <tlauratascon@gmail.com>
2022-11-02 07:06:36 -0500

    tallerGeometria.js

--- TallerGeometria.js
+++ TallerGeometria.js
@@ -22,16 +22,48 @@ function division(num1,num2){
     //return mensaje
     return resultado
 }
-function potensia (num1,num2){
+function potencia (num1,num2){
     var resultado = num1 ** num2;
     var mensaje = "el resultado de potensia "+ num1 + "y" + num2 + "es" + resultado;
    // return mensaje
     return resultado
-
+}
 function main_geometria(){
     var eleccion = prompt("por favor escriba el numero de la figura que desea realizar: \n 1.cuadrado \n 2.rectangulo")
 
     var eleccion2 = prompt("por favor escriba si desea hallar area o perimetro : \n 1.area \n 2.perimetro")
+
+    var numero1 = parseFloat(prompt("por favor ingrese el ancho de la figura:"));
+    var numero2 = parseFloat(prompt("porfavor ingrese el alto de la figura"));
+
+    if(eleccion == 1){
+        if(eleccion2 == 1){
+           console.log("el area del cuadrado es " +potencia(numero1,2))
+
+        }
+
+    }else if(eleccion ==2){
+        if(eleccion2 == 1){
+            console.log("el area del rectanguloc es " +multiplicacion(numero2,numero1))
+ 
+         }
+
+    }else if(eleccion == 1){
+    if(eleccion2 == 2){
+       console.log("el perimetro del cuadrado es " +multiplicacion(numero1,4))
+       
+          }
+    
+    }else if(eleccion == 2){
+        if(eleccion2 == 2){
+        var resultadoaltura = multiplicacion(numero1,2);
+        var resultadobase = multiplicacion(numero2,2);
+           console.log("el perimetro del rectangulo es " +suma(resultadoaltura,resultadobase))
+            
+           }
+
+    }
+
 
 
 
--- calculadora.js
+++ calculadora.js
@@ -31,11 +31,11 @@ function potensia (num1,num2){
 function main_calculadora(){
     var eleccion = prompt("por favor ingrese el numero de la operacion que desea realizar: \n 1.suma \n 2.resta \n 3.division \n 4.multiplicacion \n 5.potencia\n 6.doble de una suma \n 7.mitad de un producto ");
 
-var numero1 = parseFloat(prompt)
-    (("por favor ingrese el primer valor:"));
-var numero2 = parseFloatprompt(("porfavor ingrese el segundo vlor"));
+var numero1 = parseFloat(prompt("por favor ingrese el primer valor:"));
+var numero2 = parseFloat(prompt("porfavor ingrese el segundo vlor"));
 
 if(eleccion == 1){
+
 console.log(suma(numero1, numero2));
 } else  if(eleccion ==2 ){
 
@@ -44,6 +44,7 @@ console.log(suma(numero1, numero2));
 
 }else if(eleccion == 6){
     var resultadoSuma = suma(numero1,numero2);
+    
     console.log("El doble de la suma de" + numero1 + "y" + numero2 + "es " + multiplicacion(resultadoSuma,2))
 
 }else if (eleccion ==7){
